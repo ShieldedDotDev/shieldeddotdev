6401e28a234b57f2793e50d4c912a2621380766d
Jesse Donat <donatj@gmail.com>
2020-01-14 23:54:41 -0600

    Adds basic crud to shields model

--- model/shields.go
+++ model/shields.go
@@ -46,6 +46,20 @@ func (sm *ShieldMapper) GetFromID(id int64) (*Shield, error) {
 	}
 }
 
+func (sm *ShieldMapper) GetFromUserIDAndID(userID, id int64) (*Shield, error) {
+	sh := &Shield{}
+
+	row := sm.db.QueryRow("SELECT s.shield_id, s.user_id, s.name, s.title, s.text, s.color, s.secret FROM shields AS s WHERE s.user_id = ? AND s.shield_id = ?", userID, id)
+	switch err := row.Scan(&sh.ShieldID, &sh.UserID, &sh.Name, &sh.Title, &sh.Text, &sh.Color, &sh.Secret); err {
+	case sql.ErrNoRows:
+		return nil, nil
+	case nil:
+		return sh, nil
+	default:
+		return nil, err
+	}
+}
+
 func (sm *ShieldMapper) GetFromSecret(secret string) (*Shield, error) {
 	sh := &Shield{}
 
@@ -80,4 +94,69 @@ func (sm *ShieldMapper) GetFromUserID(userID int64) ([]*Shield, error) {
 	}
 
 	return out, nil
+}
+
+func (sm *ShieldMapper) Save(n *Shield) error {
+	success := false
+
+	tx, err := sm.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if success {
+			tx.Commit()
+		} else {
+			tx.Rollback()
+		}
+	}()
+
+	if n.ShieldID > 0 {
+		_, err = tx.Exec("UPDATE shields SET user_id=?, name=?, title=?, text=?, color=?, secret=? WHERE shield_id = ?",
+			n.UserID, n.Name, n.Title, n.Text, n.Color, n.Secret, n.ShieldID)
+		if err != nil {
+			return err
+		}
+	} else {
+		res, err := tx.Exec("INSERT INTO shields (user_id, name, title, text, color, secret) VALUES (?,?,?,?,?,?)",
+			n.UserID, n.Name, n.Title, n.Text, n.Color, n.Secret)
+		if err != nil {
+			return err
+		}
+
+		i, err := res.LastInsertId()
+		if err != nil {
+			return err
+		}
+
+		n.ShieldID = i
+	}
+
+	success = true
+
+	return nil
+}
+
+func (sm *ShieldMapper) Delete(n *Shield) error {
+	success := false
+	tx, err := sm.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if success {
+			tx.Commit()
+		} else {
+			tx.Rollback()
+		}
+	}()
+
+	_, err = tx.Exec("DELETE FROM shields WHERE shield_id = ?", n.ShieldID)
+	if err != nil {
+		return err
+	}
+
+	success = true
+
+	return nil
 }
