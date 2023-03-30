be0fba1bbb4689cd68cf7d3743ea37a1f2817f00
Jesse Donat <donatj@gmail.com>
2023-03-30 16:58:07 -0500

    Add privacy policy

--- pages/_footer.php
+++ pages/_footer.php
@@ -1,3 +1,4 @@
 <footer>
 	<a href="https://github.com/shieldedDotDev/">Shielded.dev on GitHub</a>
+	<a href="/privacy.html">Privacy Policy</a>
 </footer>
---
+++ pages/privacy.html.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+
+<head>
+	<?php require('_head.php') ?>
+</head>
+
+<body class="home--app">
+	<?php require('_header.php') ?>
+	<main>
+		<section>
+			<article>
+				<h1 id="privacy-policy">Privacy Policy</h1>
+				<p>This Privacy Policy explains how we collect, use, and disclose information that we receive from visitors to our website. By using our website, you agree to the terms of this Privacy Policy.</p>
+				<h2 id="information-we-collect">Information We Collect</h2>
+				<p>When you use our website, we may collect certain information from you, including your IP address, browser type, operating system, and other technical information about your device. We may also collect information about your use of our website, such as the pages you visit and the features you use.</p>
+				<p>We use cookies to handle authorization and the handshake process with GitHub. No tracking cookies are used.</p>
+				<h2 id="use-of-information">Use of Information</h2>
+				<p>We use the information we collect to operate and improve our website, to provide you with a better experience when using our website, and to communicate with you about our website.</p>
+				<p>We may also use the information we collect for other purposes, such as to enforce our terms of service, to comply with legal obligations, or to protect our rights and interests.</p>
+				<h2 id="disclosure-of-information">Disclosure of Information</h2>
+				<p>We do not share your personal information with third parties, except as necessary to operate our website or to comply with legal obligations. We will never sell your data to third parties.</p>
+				<p>We may also disclose information about you if we believe that disclosure is necessary to protect our rights or interests, to enforce our terms of service, or to comply with legal obligations.</p>
+				<h2 id="data-posted-to-our-website">Data Posted to Our Website</h2>
+				<p>All data that you submit via our website or API, other than your login details (such as your email address and GitHub account), should be considered publicly posted. We are a website that hosts README shields (aka badges) and their contents should be considered posted in a public forum.</p>
+				<h2 id="log-retention">Log Retention</h2>
+				<p>We make a good faith effort not to retain logs containing IP addresses longer than 90 days.</p>
+				<h2 id="data-security">Data Security</h2>
+				<p>We take reasonable measures to protect your information from unauthorized access, use, or disclosure. However, no method of transmitting information over the internet or storing information is completely secure. Therefore, we cannot guarantee the absolute security of your information.</p>
+				<h2 id="location-and-age-restrictions">Location and Age Restrictions</h2>
+				<p>Our website is hosted in the United States of America and is directed to people who are at least 13 years old. If you are under the age of 13, per the requirements of COPPA (Children&#39;s Online Privacy Protection Act), do not use this site. In the European Union, users must be at least 18 years of age. We reserve the right to deny service to anyone under these age limits and to evict any data found to be in conflict with these restrictions.</p>
+				<h2 id="changes-to-privacy-policy">Changes to Privacy Policy</h2>
+				<p>We may update this Privacy Policy from time to time by posting a new version on our website. You should check this page periodically to ensure that you are aware of any changes.</p>
+				<h2 id="effective-date">Effective Date</h2>
+				<p>This Privacy Policy is effective as of 2023-03-30.</p>
+			</article>
+		</section>
+	</main>
+	<?php require('_footer.php') ?>
+</body>
+
+</html>
