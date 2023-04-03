13d39c029e9d44b5693a94a57aaf29f24bc6ea37
Jesse Donat <donatj@gmail.com>
2023-04-03 15:35:08 -0500

    Improves theme color

--- pages/_head.php
+++ pages/_head.php
@@ -2,7 +2,7 @@
 <link rel="dns-prefetch" href="//api.shielded.dev">
 <link rel="dns-prefetch" href="//img.shielded.dev">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-<meta name="theme-color" content="#ff5e86" />
+<meta name="theme-color" content="#5c72a6" />
 <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
 <title>Shielded.dev</title>
 <link rel="stylesheet" href="style/style.css" type="text/css" />
